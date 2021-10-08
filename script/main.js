f528a9fbbd2a459fbc9d486daac8a35ef351ed06
Suyash <suyashsingh.stem@gmail.com>
2021-10-08 18:14:43 +0530

    Added Javascript for movement

--- script/main.js
+++ script/main.js
@@ -2,6 +2,35 @@ function restart(){
     location.reload();
     return false;
 }
+
+const playerShip = document.querySelector("#curShip");
+
+const playerShipdimension = playerShip.getBoundingClientRect();
+
+const shipSpeed = 8;
+
+document.addEventListener('keydown', (e) => {
+    
+    switch(e.code)
+    {
+
+    case "ArrowLeft":
+        if( playerShipdimension[left] > 5 )
+        {
+            playerShipdimension[left] -= shipSpeed; 
+        }
+    break;
+
+    case "ArrowRight":
+        if( playerShipdimension[right] < 1004 )
+        {
+            playerShipdimension[right] -= shipSpeed;
+        }
+    }
+
+});
+
+
 let ship1=document.getElementById("ship1");
 let ship2=document.getElementById("ship2");
 let ship3=document.getElementById("ship3");
@@ -20,3 +49,5 @@ function chooseShip(spaceship){
     document.getElementById("curShip").src=spaceship;
 }
 
+document.addEventListener("keydown", controller.keyListner);
+document.addEventListener("keydown", controller.keyListner);    
