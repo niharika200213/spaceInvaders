90c82a30a84352aab1666ee1a83d8c338f8c51be
niharika rastogi <rastoginiha2002@gmail.com>
2021-10-08 17:49:32 +0530

    added choose ship feature

--- script/main.js
+++ script/main.js
@@ -7,15 +7,16 @@ let ship2=document.getElementById("ship2");
 let ship3=document.getElementById("ship3");
 
 document.getElementById("ship1").addEventListener("click",function(){
-    chooseShip(ship1);}
+    chooseShip("images/ship1.png");}
     );
 document.getElementById("ship2").addEventListener("click",function(){
-    chooseShip(ship2);}
+    chooseShip("images/ship2.png");}
     );
 document.getElementById("ship3").addEventListener("click",function(){
-    chooseShip(ship3);}
+    chooseShip("images/ship3.png");}
     );
 
 function chooseShip(spaceship){    
-    document.getElementById("curShip").innerHTML=spaceship;
+    document.getElementById("curShip").src=spaceship;
 }
+
