44bed2b4c0ba32ed38325469d9012620e37be34f
niharika rastogi <rastoginiha2002@gmail.com>
2021-10-08 17:08:25 +0530

    added spaceships

--- script/main.js
+++ script/main.js
@@ -2,3 +2,9 @@ function restart(){
     location.reload();
     return false;
 }
+
+function chooseShip(){
+    var ship1=document.getElementById("ship1");
+    var ship2=document.getElementById("ship2");
+    var ship3=document.getElementById("ship2");
+}
