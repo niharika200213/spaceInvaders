cc8343380b4c0efd2698880dbbb4a96f3752ca4b
Suyash <suyashsingh.stem@gmail.com>
2021-10-08 19:06:13 +0530

    improved code readibility

--- script/main.js
+++ script/main.js
@@ -4,33 +4,24 @@ function restart(){
 }
 
 const playerShip = document.querySelector("#curShip");
-
 const playerShipdimension = playerShip.getBoundingClientRect();
-
-var x = playerShipdimension.left;
-
+var x = playerShipdimension.left
 const shipSpeed = 7;
 
 document.addEventListener('keydown', (e) => {
-    
     switch(e.code)
     {
-
     case "ArrowLeft":
-
         console.log("foo");
-        if( x > 15 )
-        {
+        if( x > 15 ){
             x -= shipSpeed; 
             document.getElementById("curShip").style.left = x + "px";
         }
     break;
 
     case "ArrowRight":
-
         console.log("fee");
-        if( x < 985)
-        {
+        if( x < 985){
             x += shipSpeed;
             document.getElementById("curShip").style.left = x + "px";
         }
