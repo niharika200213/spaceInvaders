60d620e45ac101200c82f761464f7ace9dbd9dfd
niharika rastogi <rastoginiha2002@gmail.com>
2021-10-08 17:42:23 +0530

    added current spaceship in game area

--- script/main.js
+++ script/main.js
@@ -2,9 +2,20 @@ function restart(){
     location.reload();
     return false;
 }
+let ship1=document.getElementById("ship1");
+let ship2=document.getElementById("ship2");
+let ship3=document.getElementById("ship3");
 
-function chooseShip(){
-    var ship1=document.getElementById("ship1");
-    var ship2=document.getElementById("ship2");
-    var ship3=document.getElementById("ship2");
+document.getElementById("ship1").addEventListener("click",function(){
+    chooseShip(ship1);}
+    );
+document.getElementById("ship2").addEventListener("click",function(){
+    chooseShip(ship2);}
+    );
+document.getElementById("ship3").addEventListener("click",function(){
+    chooseShip(ship3);}
+    );
+
+function chooseShip(spaceship){    
+    document.getElementById("curShip").innerHTML=spaceship;
 }
