f0fc98d91b56b6bc30637890c9df583272cfa403
niharika rastogi <rastoginiha2002@gmail.com>
2021-10-10 17:20:35 +0530

    added lives and collide function

--- script/main.js
+++ script/main.js
@@ -3,15 +3,21 @@ function restart(){
     return false;
 }
 
-const playerShip = document.querySelector("#curShip");
+document.getElementById("ship1").addEventListener("click",function(){
+    chooseShip("images/ship1.png");}
+    );
+document.getElementById("ship2").addEventListener("click",function(){
+    chooseShip("images/ship2.png");}
+    );
+document.getElementById("ship3").addEventListener("click",function(){
+    chooseShip("images/ship3.png");}
+    );
 
-const ENEMIES_PER_ROW = 10;
-const ENEMY_HORIZONTAL_PADDING = 80;
-const ENEMY_VERTICAL_PADDING = 70;
-const ENEMY_VERTICAL_SPACING = 80;
+function chooseShip(spaceship){    
+    document.getElementById("curShip").src=spaceship;
+}
 
-const GAME_WIDTH = 800;
-const GAME_HEIGHT = 600;
+let playerShip = document.querySelector("#curShip");
 
 const STATE = {
     x_pos : 0,
@@ -20,15 +26,14 @@ const STATE = {
     enemyLasers: [],
     enemies : [],
     number_of_enemies: 24,
-    counter: 0
-  }
+    counter: 0,
+    lives: 3
+}
 
 var x = 500;
 var y = 5;
 
-
 const shipSpeed = 8;
-
 
 let Keys = {
     up: false,
@@ -72,20 +77,6 @@ function move(){
     }
 }
 
-document.getElementById("ship1").addEventListener("click",function(){
-    chooseShip("images/ship1.png");}
-    );
-document.getElementById("ship2").addEventListener("click",function(){
-    chooseShip("images/ship2.png");}
-    );
-document.getElementById("ship3").addEventListener("click",function(){
-    chooseShip("images/ship3.png");}
-    );
-
-function chooseShip(spaceship){    
-    document.getElementById("curShip").src=spaceship;
-}
-
 function countdown(){
     var i=5;
     var id=setInterval(cd,1000);
@@ -111,7 +102,7 @@ function countdown(){
                 move();
             });
            
-           document.addEventListener("keyup", function(e){
+            document.addEventListener("keyup", function(e){
                 var keycode = e.code;
            
                 if(keycode == "ArrowLeft") Keys.left = false;
@@ -120,8 +111,8 @@ function countdown(){
                 if(keycode == "ArrowDown") Keys.down = false;
                 if(keycode == "Space") Keys.space = false;
            });
-
-           setInterval(updateEnemiesY,100);
+           
+            setInterval(update, 20);
 
         }
         else{
@@ -145,27 +136,26 @@ function createEnemy(gameArea, x, y){
     $enemy.src = "/images/alien1.png";
     $enemy.className = "enemy";
     gameArea.appendChild($enemy);
-    const enemy_cooldown = Math.floor(Math.random()*100);
-    const enemy = {x, y, $enemy, enemy_cooldown};
+    const enemy = {x, y, $enemy};
     STATE.enemies.push(enemy);
     setPosition($enemy, x, y);
-  }
+}
   
-
 function updateEnemies()
 {
     const dx = 70*Math.cos(Date.now()/500);
     const dy = 40*Math.sin(Date.now()/500) + STATE.counter;
-
     const enemies = STATE.enemies;
-
     for (let i = 0; i < enemies.length; i++){
       const enemy = enemies[i];
       var a = enemy.x + dx;
       var b = enemy.y + dy;
       setPosition(enemy.$enemy, a, b);
+      if (enemy.enemy_cooldown == 0){
+        createEnemyLaser($container, a, b);
+      }
     }
-  }
+}
 
 function updateEnemiesY()
 {
@@ -173,7 +163,6 @@ function updateEnemiesY()
 }
 
 function createEnemies(gameArea) {
-
     for(var i = 0; i <= STATE.number_of_enemies/3; i++){
       createEnemy(gameArea, i*80, 100);
     } 
@@ -182,17 +171,33 @@ function createEnemies(gameArea) {
     }
     for(var i = 0; i <= STATE.number_of_enemies/3; i++){
         createEnemy(gameArea, i*80, 260);
-      }
+    }
 }
-
 
 const gameArea = document.querySelector("#gameArea");
 createEnemies(gameArea);
 
-setInterval(update, 20);
-setInterval(update, 500);
+setInterval(updateEnemies,10);
 
 function update(){
     updateEnemiesY();
     updateEnemies(gameArea);
+}
+
+function collideRect(rect1, rect2){
+    return!(rect2.left > rect1.right || 
+      rect2.right < rect1.left || 
+      rect2.top > rect1.bottom || 
+      rect2.bottom < rect1.top);
+}
+
+function lives(){
+    if(STATE.lives>0){
+        document.getElementById(`star${STATE.lives}`).display="none";
+        STATE.lives--;
+    }
+    else{
+        document.getElementById("countdown").style.fontSize=50+"px";
+        document.getElementById("countdown").innerHTML="You Lost! Restart";
+    }
 }
